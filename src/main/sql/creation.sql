bda5c84d0722ab54393e9c923bd13d74c6f0f71f
Dave <david.craciunescu@edu.uah.es>
2021-01-28 10:49:07 +0100

    Adds database creation script

---
+++ src/main/sql/creation.sql
@@ -0,0 +1,129 @@
+
+DROP TABLE IF EXISTS USERS;
+CREATE TABLE USERS
+(
+    user_id integer NOT NULL,
+    user_name text NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+ALTER TABLE USERS
+    OWNER to postgres;
+
+DROP TABLE IF EXISTS TAGS;
+CREATE TABLE TAGS
+(
+    tag_id integer NOT NULL,
+    name text NOT NULL,
+    PRIMARY KEY (tag_id)
+);
+
+ALTER TABLE TAGS
+    OWNER to postgres;
+
+DROP TABLE IF EXISTS AUTHORS;
+CREATE TABLE AUTHORS
+(
+    author_id integer NOT NULL,
+    name text NOT NULL,
+    PRIMARY KEY (author_id)
+);
+
+ALTER TABLE AUTHORS
+    OWNER to postgres;
+
+DROP TABLE IF EXISTS MEDIA;
+CREATE TABLE MEDIA
+(
+    media_id integer NOT NULL,
+    name text NOT NULL,
+    title text NOT NULL,
+    PRIMARY KEY (media_id)
+);
+
+ALTER TABLE MEDIA
+    OWNER to postgres;
+
+DROP TABLE IF EXISTS MEDIA_TYPE;
+CREATE TABLE MEDIA_TYPE
+(
+	type_id integer NOT NULL,
+	media_id integer NOT NULL,
+	PRIMARY KEY (type_id),
+	CONSTRAINT fk_media_id
+		FOREIGN KEY (media_id)
+			REFERENCES MEDIA(media_id)
+);
+
+ALTER TABLE MEDIA_TYPE
+    OWNER to postgres;
+
+DROP TABLE IF EXISTS MEDIA_TAGS;
+CREATE TABLE MEDIA_TAGS
+(
+    media_id integer NOT NULL,
+    tag_id   integer NOT NULL,
+    PRIMARY KEY (media_id, tag_id),
+	CONSTRAINT fk_tags_media_id
+		FOREIGN KEY (media_id)
+			REFERENCES MEDIA(media_id),
+	CONSTRAINT fk_tags_tag_id
+		FOREIGN KEY (tag_id)
+			REFERENCES TAGS(tag_id)
+);
+
+ALTER TABLE MEDIA_TAGS
+    OWNER to postgres;
+	
+DROP TABLE IF EXISTS AUTHORS_MEDIA;
+CREATE TABLE AUTHORS_MEDIA
+(
+    author_id integer NOT NULL,
+    media_id   integer NOT NULL,
+    PRIMARY KEY (author_id, media_id),
+	CONSTRAINT fk_authors_author_id
+		FOREIGN KEY (author_id)
+			REFERENCES AUTHORS(author_id),
+	CONSTRAINT fk_authors_media_id
+		FOREIGN KEY (media_id)
+			REFERENCES MEDIA(media_id)
+);
+	
+ALTER TABLE AUTHORS_MEDIA
+    OWNER to postgres;
+	
+DROP TABLE IF EXISTS USER_SCORE;	
+CREATE TABLE USER_SCORE
+(
+    user_id integer NOT NULL,
+    media_id   integer NOT NULL,
+	score		integer NOT NULL,
+    PRIMARY KEY (user_id, media_id),
+	CONSTRAINT fk_userscore_user_id
+		FOREIGN KEY (user_id)
+			REFERENCES USERS(user_id),
+	CONSTRAINT fk_userscore_media_id
+		FOREIGN KEY (media_id)
+			REFERENCES MEDIA(media_id)
+);
+
+ALTER TABLE USER_SCORE
+    OWNER to postgres;
+	
+DROP TABLE IF EXISTS USER_WATCHED;
+CREATE TABLE USER_WATCHED
+(
+    user_id integer NOT NULL,
+    media_id   integer NOT NULL,
+	date_watched timestamp NOT NULL,
+    PRIMARY KEY (user_id, media_id),
+	CONSTRAINT fk_userwatched_user_id
+		FOREIGN KEY (user_id)
+			REFERENCES USERS(user_id),
+	CONSTRAINT fk_userwatched_media_id
+		FOREIGN KEY (media_id)
+			REFERENCES MEDIA(media_id)
+);
+
+ALTER TABLE USER_WATCHED
+    OWNER to postgres;
